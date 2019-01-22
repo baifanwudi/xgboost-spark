ab9d0230048362a4b3d8c26d6d2946a29128be69
baifan <baifan@adups.com>
2019-01-22 18:00:26 +0800

    增加checkpoint和并行度参数

---
+++ src/main/java/com/demo/cross/XgboostCrossDemo.java
@@ -0,0 +1,111 @@
+package com.demo.cross;
+
+import com.demo.base.AbstractSparkSql;
+import com.demo.util.CommonUtil;
+import ml.dmlc.xgboost4j.scala.spark.XGBoostClassificationModel;
+import ml.dmlc.xgboost4j.scala.spark.XGBoostEstimator;
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator;
+import org.apache.spark.ml.feature.VectorAssembler;
+import org.apache.spark.ml.param.ParamMap;
+import org.apache.spark.ml.tuning.CrossValidator;
+import org.apache.spark.ml.tuning.CrossValidatorModel;
+import org.apache.spark.ml.tuning.ParamGridBuilder;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author allen
+ * @date 2019/1/21.
+ */
+public class XgboostCrossDemo extends AbstractSparkSql {
+
+	@Override
+	public void executeProgram(String[] args, SparkSession spark) throws IOException {
+		Dataset<Row> tableData=spark.sql("select * from tmp_trafficwisdom.ml_train_data where future_day>0 ")
+				.drop("userid,city,from_place,to_place,start_city_name,end_city_name,start_city_id,end_city_id".split(","));
+
+		String[] features=new String[]{
+				"category", "future_day",
+				"banner_min_time","banner_min_price",
+				"page_train", "page_flight", "page_bus",
+				"page_transfer",
+				"start_end_distance", "total_transport", "high_railway_percent", "avg_time", "min_time",
+				"avg_price", "min_price",
+				"label_05060801", "label_05060701", "label_05060601", "label_02050601", "label_02050501", "label_02050401",
+				"is_match_category", "train_consumer_prefer", "flight_consumer_prefer", "bus_consumer_prefer"
+		};
+
+		VectorAssembler assembler = new VectorAssembler().setInputCols(features).setOutputCol("features");
+		Dataset<Row> trainData=assembler.transform(tableData).select("features","isclick");
+		trainData.cache();
+
+		Map<String,Object> javaMap=new HashMap<>(12);
+		javaMap.put("objective","binary:logistic");
+		//learning_rate
+		javaMap.put("eta",0.1);
+		javaMap.put("max_depth",9);
+		javaMap.put("min_child_weight",5);
+		//L2
+		javaMap.put("lambda",0.2);
+		javaMap.put("eval_metric","logloss");
+		javaMap.put("num_round","150");
+		javaMap.put("missing",-99);
+		//default 1,提高并行度
+		javaMap.put("nworkers",32);
+		javaMap.put("numEarlyStoppingRounds",20);
+		javaMap.put("checkpoint_path","/data/twms/traffichuixing/checkpoint/xgboost");
+		CommonUtil.toScalaImmutableMap(javaMap);
+		XGBoostEstimator xgBoostEstimator=new XGBoostEstimator( CommonUtil.<String,Object>toScalaImmutableMap(javaMap))
+				.setFeaturesCol("features").setLabelCol("isclick");
+
+		BinaryClassificationEvaluator evaluator=new BinaryClassificationEvaluator().setLabelCol("isclick").setRawPredictionCol("probabilities");
+
+
+		ParamMap[] paramGrid=new ParamGridBuilder()
+				.addGrid(xgBoostEstimator.maxDepth(),new int[]{4,6,8})
+				.addGrid(xgBoostEstimator.eta(),new double[]{0.1,0.2,0.3})
+				.addGrid(xgBoostEstimator.lambda(),new double[]{0.1,0.3,0.6})
+//				.addGrid(xgBoostEstimator.minChildWeight(),new double[]{1.0,3.0,5.0})
+				.build();
+
+		CrossValidator crossValidator=new CrossValidator().setEstimator(xgBoostEstimator)
+				.setEvaluator(evaluator).setEstimatorParamMaps(paramGrid).setNumFolds(4);
+
+		CrossValidatorModel cvModel=crossValidator.fit(trainData);
+
+		XGBoostClassificationModel xgBestModel=(XGBoostClassificationModel)(cvModel.bestModel());
+		ParamMap[] paramMaps=cvModel.getEstimatorParamMaps();
+		double[] aucAreas=cvModel.avgMetrics();
+
+		for(int i=0;i<paramMaps.length;i++){
+			System.out.println("--------------"+i+"----------------");
+			System.out.println("param:"+paramMaps[i]);
+			System.out.println("auc metric is :"+aucAreas[i]);
+		}
+
+		System.out.println("---------best param is :---------------");
+		System.out.println(xgBestModel.extractParamMap());
+
+
+		trainData.unpersist();
+
+		Dataset<Row> testData=assembler.transform(spark.sql("select * from tmp_trafficwisdom.ml_test_data "));
+		Dataset<Row> testResult=xgBestModel.transform(testData);
+
+		Double aucArea=evaluator.evaluate(testResult);
+		System.out.println("------------------test result predict----------------");
+		System.out.println("auc is :"+aucArea);
+
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		XgboostCrossDemo xgboostCrossDemo=new XgboostCrossDemo();
+		xgboostCrossDemo.runAll(args,true);
+	}
+}
---
+++ src/main/java/com/demo/ml/XgboostDemoLocal.java
@@ -0,0 +1,86 @@
+package com.demo.ml;
+
+import com.demo.util.CommonUtil;
+import ml.dmlc.xgboost4j.java.XGBoostError;
+import ml.dmlc.xgboost4j.scala.Booster;
+import ml.dmlc.xgboost4j.scala.spark.XGBoostClassificationModel;
+import ml.dmlc.xgboost4j.scala.spark.XGBoostEstimator;
+import org.apache.spark.ml.Pipeline;
+import org.apache.spark.ml.PipelineModel;
+import org.apache.spark.ml.PipelineStage;
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator;
+import org.apache.spark.ml.feature.VectorAssembler;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author allen
+ * @date 2019/1/14.
+ */
+public class XgboostDemoLocal {
+
+	public static void main(String[] args) throws IOException, XGBoostError {
+
+		SparkSession spark =SparkSession.builder().master("local[*]").getOrCreate();
+
+		Dataset<Row> trainData = spark.read()
+				.option("inferschema", "true")
+				.option("header", "true")
+				.option("encoding", "gbk")
+				.csv("/Users/AllenBai/data/ml_offical_data.csv").drop("create_time");
+
+		trainData.printSchema();
+		trainData.show(false);
+
+		trainData.cache();
+		String[] features=new String[]{
+				"category", "future_day",
+				"banner_min_time","banner_min_price",
+				"page_train", "page_flight", "page_bus",
+				"page_transfer",
+				"start_end_distance", "total_transport", "high_railway_percent", "avg_time", "min_time",
+				"avg_price", "min_price",
+				"label_05060801", "label_05060701", "label_05060601", "label_02050601", "label_02050501", "label_02050401",
+				"is_match_category", "train_consumer_prefer", "flight_consumer_prefer"
+				, "bus_consumer_prefer"
+		};
+
+		VectorAssembler assembler = new VectorAssembler().setInputCols(features).setOutputCol("features");
+		Map<String,Object> javaMap=new HashMap<>();
+		javaMap.put("objective","binary:logistic");
+		javaMap.put("eta",0.1);
+		javaMap.put("max_depth",9);
+		javaMap.put("min_child_weight",5);
+		javaMap.put("alpha",1);
+		javaMap.put("eval_metric","logloss");
+		javaMap.put("num_round","20");
+		javaMap.put("missing",-1);
+
+		CommonUtil.toScalaImmutableMap(javaMap);
+		XGBoostEstimator xgBoostEstimator=new XGBoostEstimator( CommonUtil.<String,Object>toScalaImmutableMap(javaMap))
+				.setFeaturesCol("features").setLabelCol("isclick");
+
+		Pipeline pipeline = new Pipeline()
+				.setStages(new PipelineStage[]{assembler,xgBoostEstimator});
+
+		PipelineModel pipelineModel = pipeline.fit(trainData);
+
+		Dataset<Row> predictResult=pipelineModel.transform(trainData);
+		predictResult.show(false);
+
+		XGBoostClassificationModel xgBoostClassificationModel=(XGBoostClassificationModel) (pipelineModel.stages()[1]);
+
+		//evaluate
+		BinaryClassificationEvaluator evaluator=new BinaryClassificationEvaluator().setLabelCol("isclick").setRawPredictionCol("probabilities");
+		Double aucArea=evaluator.evaluate(predictResult);
+		System.out.println("auc is :"+aucArea);
+
+//		pipelineModel.write().overwrite().save("file:///model/xgboost/pipemodel");
+
+	}
+}
